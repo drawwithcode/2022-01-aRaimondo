f0d15c10eb7765183ef253f6e46b3523057d9db4
aRaimondo <ale_1008@hotmail.it>
2022-09-27 18:19:58 +0200

    Beginning of draw function, creating circle by rotation of two strokes

--- sketch.js
+++ sketch.js
@@ -10,3 +10,18 @@ function setup() {
   frameRate(40);
 
 }
+
+function draw() {
+
+  push()                                     //creazione del cerchio centrale dalla rotazione di due segmenti
+    translate(width/2, height/2)
+    rotate(angle);
+    stroke("#C98E4A");
+    line(0, 0, 100, 0);
+    stroke("#97AD3C")
+    line(100, 0, 150, 0);
+    if(angle<360){                            //l'angolo aumenta finché non ne raggiunge uno specifico
+      angle = angle + 1;
+    }
+  pop() 
+  }

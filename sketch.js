4b43acca6a2851f5d49379bc5a0c04c78bcf55be
aRaimondo <ale_1008@hotmail.it>
2022-09-27 18:09:30 +0200

    Declaring variable

--- sketch.js
+++ sketch.js
@@ -1,13 +1,3 @@
-function preload() {
-  // put preload code here
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-}
-
-function draw() {
-  // put drawing code here
-  background("red");
-}
+let angle = 0;      //angolo del primo cerchio
+let direzione = "destra";
+let x = 200;

d7978aa6890c17a266cdd21374a614a05393b8d0
aRaimondo <ale_1008@hotmail.it>
2022-09-27 18:18:12 +0200

    function setup, setting anglemode, rectmode, frameRate and dimension and color of background

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
 let angle = 0;      //angolo del primo cerchio
 let direzione = "destra";
 let x = 200;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER)
+  angleMode(DEGREES)
+  background("#AE412A");
+  frameRate(40);
+
+}
